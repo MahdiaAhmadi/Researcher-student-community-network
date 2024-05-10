4bf93439e96d9fd39bae85175aa1b48af5d64e30
jgmsilva <jgui.silva1510@gmail.com>
2024-05-10 12:07:19 +0100

    add default picture to profile

--- src/components/ProfileDescription.jsx
+++ src/components/ProfileDescription.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image"
-
+import profilePicture from "./106x87.png"
 const ProfileDescription = ({ name, education, country }) => {
   return (
     <div className="flex w-screen flex-wrap py-3">
-      <Image className="w-[100px] h-[85px]left-0 top-[33px]" alt="Profile picture of the user"/>
-      <div className="w-1/5">
+      <Image className="left-0 top-[33px] p-2" alt="Profile picture of the user" src={profilePicture} width={106} height={87}/>
+      <div className="pl-2 w-1/5">
         <div>{name}</div>
         <div>{education}</div>
         <div>{country}</div>
