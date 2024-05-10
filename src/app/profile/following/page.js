b2f2c3bb99d12340527a69135fcb1c053045dc33
jgmsilva <jgui.silva1510@gmail.com>
2024-05-10 12:08:12 +0100

    initial implementation of followed research

--- src/app/profile/following/page.js
+++ src/app/profile/following/page.js
@@ -4,12 +4,15 @@ import { ProfileNavigation } from "@/components/ProfileNavigation"
 import { FollowingTable } from "@/components/FollowingTable"
 import { signIn, useSession } from "next-auth/react";
 export default function StatsConsult() {
+    const rl = [ "research1", "research2" ]
+    const ql = [ "question1", "question2" ]
+    const tl = [ "topic1", "topic2" ]
     return (
         <div className="fixed">
           <ProfileDescription name="First Last" education="IPB" country="Portugal"/>
           <ProfileNavigation/>
           <div className="flex flex-cols py-10 justify-between max-w-3xl xl:max-w-5xl">
-            <FollowingTable />
+            <FollowingTable researchList={rl} questionList={ql} topicList={tl}/>
           </div>
         </div>
     );
--- src/components/FollowingTable.jsx
+++ src/components/FollowingTable.jsx
@@ -1,19 +1,55 @@
-const FollowingTable = () => {
-  const informationMap = { "pub":2 }
+import { useState } from "react"
+const FollowingTable = ({ researchList, questionList, topicList }) => {
+  const [FollowState, setFollowState] = useState(1);
+
+  let displayList = [  ];
+  switch (FollowState) {
+    case 0:
+      displayList = researchList.concat(questionList)
+      break;
+    case 1:
+      displayList = researchList
+      break;
+    case 2:
+      displayList = questionList
+      break;
+  }
   return (
-    <div className="w-1/2 space-y-2">
-      <div className=" w-5/6 bg-white border border-black items-center py-2">
-        <div className="ml-2 text-black">Following</div>
+    <div className="flex flex-cols w-full">
+      <div className=" w-1/4 space-y-2">
+        <div className="w-full bg-white border border-black items-center py-2">
+          <div className="ml-2 text-black">Following</div>
+        </div>
+        <div className=" font-bold text-black w-full bg-white border border-black py-2 items-center">
+          <button className="ml-4 hover:underline" onClick={ () => {setFollowState(0)}}>Research ({researchList.length + questionList.length})</button>
+          <button className="ml-8 hover:underline" onClick={() => {setFollowState(1)}}>Research Item ({researchList.length})</button>
+          <button className="ml-8 hover:underline" onClick={() => {setFollowState(2)}}>Question ({questionList.length})</button>
+          <button className="ml-4 py-2 hover:underline" >Topic ({topicList.length})</button>
+        </div>
       </div>
-      <div className="w-5/6 bg-white border border-black items-center py-2 items-center">
-        { Object.keys(informationMap).map((key) => {
-          return (
+      <div className="items-center w-full">
+        <div className="font-14 text-center py-2 text-xl text-bold">Research You Followed</div>
+        <div className="bg-white border border-black mb-8 mx-8">
+          { displayList.map( (elem) => { return (
             <div>
-              <div className="text-black ml-4 font-bold">{key}</div>
-              <div className="text-black ml-6">value</div>
+              <div className=" text-black font-lg p-4">{elem}</div>
+              <div className="flex justify-end text-black font-light p-4">
+                <button className="hover:underline">Recommend</button>
+                <button className="px-4 hover:underline">Follow</button>
+                <button className="hover:underline">Share</button>
+              </div>
             </div>
-          )
-        }) }
+          ) } )}
+        </div>
+        <div>
+          <div className="bg-white border border-black p-4 shadow mx-8 flex justify-between">
+            <div className="text-neutral-800 ">Topics You Followed ({topicList.length})</div>
+            <button className="text-black hover:underline">Edit</button>
+          </div>
+          <div className="bg-white border border-black mx-8 flex justify-center mt-2 p-4">
+            { topicList.map( (elem) => { return (<div className=" mx-2 p-3 bg-zinc-300 rounded-full text-black text-center">{elem}</div>) } )}
+          </div>
+        </div>
       </div>
     </div>
   )
