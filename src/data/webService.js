88e8fa575ec5c17cee1aa9f15932c0360a18282e
Felipe Bueno de Souza <a61491@alunos.ipb.pt>
2024-05-25 16:43:18 +0100

    register new user fixed

--- src/data/webService.js
+++ src/data/webService.js
@@ -25,6 +25,26 @@ function internalPost(url, body) {
     return fetch(baseURL + url, requestOptions)
 }
 
+
+function internalPut(url, body) {
+    var requestOptions = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(body)
+    };
+
+    return fetch(baseURL + url, requestOptions)
+}
+
+export async function put(url, body) {
+    return internalPut(url, body)
+        .then(res => res.json())
+        .then(handleJsonResponse)
+}
+
 export async function get(url) {
     return internalGet(url)
         .then(res => res.json())
@@ -33,6 +53,7 @@ export async function get(url) {
 
 export async function post(url, body) {
     return internalPost(url, body, true)
+        .then(res => res.json())
         .then(handleJsonResponse)
 }
 
