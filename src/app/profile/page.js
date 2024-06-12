2d85fabca961766d89feb171511d1f1d42f7318a
jgmsilva <jgui.silva1510@gmail.com>
2024-06-12 22:21:28 +0100

    initial editable description (no connection to database yet)

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,12 +2,14 @@ import { ProfileDescription } from "@/components/ProfileDescription"
 import { AffiliationsTable  } from "@/components/AffiliationsTable"
 import { AboutMeTable       } from "@/components/AboutMeTable"
 export default function ProfileConsult() {
+  const description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  const skills = ["Security", "Computer Network"]
   return (
       <ProfileDescription>
-        <AboutMeTable informationMap={
-          { "Description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Skills":"Security, Computer Network" }
-        }/>
+        <AboutMeTable
+          Description={description}
+            Skills={skills}
+        />
         <AffiliationsTable institution="Instituto Politécnico de Bragança" location="Portugal" department="Department of Technology"/>
       </ProfileDescription>
   )
--- src/components/AboutMeTable.jsx
+++ src/components/AboutMeTable.jsx
@@ -1,18 +1,47 @@
-const AboutMeTable = ({ informationMap }) => {
+"use client"
+import { useState } from "react"
+
+const AboutMeTable = ({ Description, Skills }) => {
+  const [Editable, setEditable] = useState(false);
+  const [text, setText] = useState(Description);
+  const [SkillArr, setSkillArr] = useState(Skills);
+
+  function handleNewSkill() {
+    let arr = [...SkillArr]
+    arr.push("New Skill")
+    setSkillArr(arr)
+  }
+  function handleDeleteSkill(index) {
+    let arr = [...SkillArr]
+    arr.splice(index, 1)
+    setSkillArr(arr)
+  }
+
   return (
     <div className="w-1/2 space-y-2">
       <div className=" w-5/6 bg-white border border-black items-center py-2">
         <div className="ml-2 text-black">About Me</div>
       </div>
       <div className="w-5/6 bg-white border border-black items-center py-2 items-center">
-        {Object.keys(informationMap).map((key, idx) => {
-          return (
-            <div className="my-2" key={idx}>
-              <div className="text-black ml-4 font-bold">{key}</div>
-              <div className="text-black ml-6">{informationMap[key]}</div>
+            <div className="my-2">
+              <button className="text-black bg-blue-500 rounded ml-4 px-1" onClick={() => setEditable(!Editable)}>{ Editable ? "Save" : "Edit"}</button>
+              <div className="text-black ml-4 font-bold">Description</div>
+              <div className="text-black ml-6" contentEditable={Editable} suppressContentEditableWarning={true}>{text}</div>
             </div>
-          )
-        })}
+            <div className="my-2">
+              <div className="text-black ml-4 mr-6 font-bold justify-between flex">
+                <div className="inline-block">Skills</div>
+                {Editable ? <button className="inline-block bg-blue-500 rounded-full px-2" onClick={handleNewSkill}>+</button> : null }
+              </div>
+              {
+                SkillArr.map((item, index) => {
+                  return (<div className="text-black ml-6 mr-6 flex justify-between" key={index}>
+                            <div contentEditable={Editable} suppressContentEditableWarning={true} onInput={(e) => {SkillArr[index] = e.target.innerText}}>{item}</div>
+                            {Editable ? <button className="inline-block bg-red-500 rounded-full px-2" onClick={() => handleDeleteSkill(index)}>-</button> : null }
+                          </div> )
+                })
+              }
+            </div>
       </div>
     </div>
   )
