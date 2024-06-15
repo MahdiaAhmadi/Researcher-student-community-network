e8c6e0819fd1ac28057d03496ffa9aa5785b6017
jgmsilva <jgui.silva1510@gmail.com>
2024-06-15 15:54:56 +0100

    now description and skils are updated in mongodb

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,14 +1,37 @@
+"use client"
+
 import { ProfileDescription } from "@/components/ProfileDescription"
 import { AffiliationsTable  } from "@/components/AffiliationsTable"
 import { AboutMeTable       } from "@/components/AboutMeTable"
+import { put, get } from "@/data/webService";
+import { useEffect, useState } from "react";
+
+function SaveHandler({description, skills}) {
+  put("/user/me/update", { profile_description: description, skills:skills }).then((data) => {
+    console.log(data)
+  }).catch(() => alert("Error changing the password!"))
+}
+
 export default function ProfileConsult() {
-  const description ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  const skills = ["Security", "Computer Network"]
+  const [description, setDescription] = useState(null);
+  const [skills, setSkills] = useState(null);
+
+  useEffect(() => {
+    if((description === null) || (skills === null))
+      get("/user/me").then((data) => {
+        console.log(data)
+        setDescription(data.profile_description)
+        setSkills(data.skills)
+      })
+  })
   return (
       <ProfileDescription>
         <AboutMeTable
           Description={description}
-            Skills={skills}
+          SetDescription={setDescription}
+          Skills={skills}
+          SetSkills={setSkills}
+          SaveFunction={SaveHandler}
         />
         <AffiliationsTable institution="Instituto Politécnico de Bragança" location="Portugal" department="Department of Technology"/>
       </ProfileDescription>
--- src/components/AboutMeTable.jsx
+++ src/components/AboutMeTable.jsx
@@ -2,29 +2,57 @@
 import { useState } from "react"
 
 function emptysave({description, skills}) { console.log("Saving") }
+function ShowDescription({Description, SetDescription, handleEditButton, Editable}) {
+  if(Description === null) return null
+  return (
+    <div className="my-2">
+      <button className="text-black bg-blue-500 rounded ml-4 px-1" onClick={handleEditButton}>{ Editable ? "Save" : "Edit"}</button>
+      <div className="text-black ml-4 font-bold">Description</div>
+      <div className="text-black ml-6" contentEditable={Editable} suppressContentEditableWarning={true} onInput={(e) => {SetDescription(e.target.innerText); console.log(Description)}}>{(' ' + Description).slice(1)}</div>
+    </div>
+  )
+}
 // SaveFunction should be the function responsible for saving the updated user information to the database.
 // should have a parameter description for the new description text and a parameter skills for the new skills array
-const AboutMeTable = ({ Description, Skills, SaveFunction = emptysave}) => {
+const AboutMeTable = ({ Description, SetDescription, Skills, SetSkills, SaveFunction = emptysave}) => {
   const [Editable, setEditable] = useState(false);
-  const [DescriptionText, setDescriptionText] = useState(Description);
-  const [SkillArr, setSkillArr] = useState(Skills);
+  const [auxDescription, setAuxDescription] = useState(Description);
 
+
+  function ShowSkills() {
+    if(Skills === null) return null
+    return (
+    <div className="my-2">
+      <div className="text-black ml-4 mr-6 font-bold justify-between flex">
+        <div className="inline-block">Skills</div>
+        {Editable ? <button className="inline-block bg-blue-500 rounded-full px-2" onClick={handleNewSkill}>+</button> : null }
+      </div>
+      {
+        Skills.map((item, index) => {
+          return (<div className="text-black ml-6 mr-6 flex justify-between" key={index}>
+                    <div contentEditable={Editable} suppressContentEditableWarning={true} onInput={(e) => {Skills[index] = e.target.innerText}}>{item}</div>
+                    {Editable ? <button className="inline-block bg-red-500 rounded-full px-2" onClick={() => handleDeleteSkill(index)}>-</button> : null }
+                  </div> )
+        })
+      }
+    </div>
+    )
+  }
   function handleNewSkill() {
-    let arr = [...SkillArr]
+    let arr = [...Skills]
     arr.push("New Skill")
-    setSkillArr(arr)
+    SetSkills(arr)
   }
   function handleDeleteSkill(index) {
-    let arr = [...SkillArr]
+    let arr = [...Skills]
     arr.splice(index, 1)
-    setSkillArr(arr)
+    SetSkills(arr)
   }
 
   function handleEditButton() {
     if(Editable) {
-      if(Description !== DescriptionText || Skills !== SkillArr) {
-        SaveFunction({ description:DescriptionText, skills:SkillArr })
-      }
+        SetDescription(auxDescription)
+        SaveFunction({ description:Description, skills:Skills })
     }
     setEditable(!Editable)
   }
@@ -35,25 +63,9 @@ const AboutMeTable = ({ Description, Skills, SaveFunction = emptysave}) => {
         <div className="ml-2 text-black">About Me</div>
       </div>
       <div className="w-5/6 bg-white border border-black items-center py-2 items-center">
-            <div className="my-2">
-              <button className="text-black bg-blue-500 rounded ml-4 px-1" onClick={handleEditButton}>{ Editable ? "Save" : "Edit"}</button>
-              <div className="text-black ml-4 font-bold">Description</div>
-              <div className="text-black ml-6" contentEditable={Editable} suppressContentEditableWarning={true} onInput={(e) => {setDescriptionText(e.target.innerText); console.log(DescriptionText)}}>{Description}</div>
-            </div>
-            <div className="my-2">
-              <div className="text-black ml-4 mr-6 font-bold justify-between flex">
-                <div className="inline-block">Skills</div>
-                {Editable ? <button className="inline-block bg-blue-500 rounded-full px-2" onClick={handleNewSkill}>+</button> : null }
-              </div>
-              {
-                SkillArr.map((item, index) => {
-                  return (<div className="text-black ml-6 mr-6 flex justify-between" key={index}>
-                            <div contentEditable={Editable} suppressContentEditableWarning={true} onInput={(e) => {SkillArr[index] = e.target.innerText}}>{item}</div>
-                            {Editable ? <button className="inline-block bg-red-500 rounded-full px-2" onClick={() => handleDeleteSkill(index)}>-</button> : null }
-                          </div> )
-                })
-              }
-            </div>
+        {(Description === null && Skills === null) ? <div>Loading Profile Information</div> : null}
+        <ShowDescription Description={Description} SetDescription={setAuxDescription} handleEditButton={handleEditButton} Editable={Editable}/>
+        <ShowSkills/>
       </div>
     </div>
   )
